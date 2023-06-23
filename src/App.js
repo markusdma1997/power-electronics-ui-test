20fe283d5a2e74b1b2e92fe4a5d8026da76df9b9
Ma <yocong.ma.ext@siemens.com>
2023-06-23 13:18:14 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <iframe
+              title="IoT2050 UDP"
+              src="http://localhost:3000/d-solo/e9c0307e-2873-4cc4-9a74-1347e5bee177/powerelectronics?orgId=1&refresh=5s&from=1687517021785&to=1687517321786&panelId=2"
+              width="450"
+              height="200"
+              frameBorder="0">
+          </iframe>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
