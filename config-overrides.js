a8b57fb4aa7b697d44d1233b957daf41e13794e2
Ma <yocong.ma.ext@siemens.com>
2023-06-30 14:40:54 +0200

    removed fs: false from resolve.fallback

--- config-overrides.js
+++ config-overrides.js
@@ -6,7 +6,7 @@ module.exports = function override (config, env) {
     console.log('override')
     let loaders = config.resolve
     loaders.fallback = {
-        "fs": false,
+        // "fs": false,
         "child_process": false,
         "path": require.resolve("path-browserify"),
         "crypto": require.resolve("crypto-browserify"),
